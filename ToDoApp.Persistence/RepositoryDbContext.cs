73e40c6d435dbab1e2b445901d76edf71fe74b8d
Max <pogatop@yandex.ru>
2022-05-23 22:38:42 +0300

    Add db context

---
+++ ToDoApp.Persistence/RepositoryDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoApp.Domain.Entities;
+
+namespace ToDoApp.Persistence;
+
+public class RepositoryDbContext : DbContext
+{
+    public RepositoryDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    public DbSet<Note> Notes { get; set; }
+    
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder) =>
+        builder.ApplyConfigurationsFromAssembly(typeof(RepositoryDbContext).Assembly);
+}
