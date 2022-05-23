c4261d7d965a30bab396a8a19eae710dba96dc75
Max <pogatop@yandex.ru>
2022-05-23 21:11:46 +0300

    Add entities

---
+++ ToDoApp.Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoApp.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; }
+    public string DisplayName { get; set; }
+    public string Password { get; set; }
+    public DateTime DateCreation { get; set; }
+    public ICollection<Note> Notes { get; set; }
+}
