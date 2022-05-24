93c5e716bd4514ffd003a3577e430009ee21112b
Max <pogatop@yandex.ru>
2022-05-24 15:05:25 +0300

    Add automapper

---
+++ ToDoApp.Services/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ToDoApp.Contracts;
+using ToDoApp.Domain.Entities;
+
+namespace ToDoApp.Services;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Note, NoteForReadDto>();
+        CreateMap<NoteForCreationDto, Note>();
+        CreateMap<NoteForUpdateDto, Note>();
+
+        CreateMap<UserForCreationDto, User>();
+        CreateMap<User, UserForReadDto>();
+    }
+}
