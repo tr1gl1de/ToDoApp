4740739517d0dc48882b809868d1ea6c46bc80d2
Max <pogatop@yandex.ru>
2022-05-24 20:40:52 +0300

    Add controllers

---
+++ ToDoApp.Presentation/Controllers/NoteController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoApp.Contracts;
+using ToDoApp.Services.Abstraction;
+
+namespace Presentation.Controllers;
+
+[ApiController]
+[Route("api/user/{userId:guid}/notes")]
+public class NoteController : ControllerBase
+{
+    private readonly IServiceManager _serviceManager;
+
+    public NoteController(IServiceManager serviceManager)
+    {
+        _serviceManager = serviceManager;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetNotes(Guid userId, CancellationToken cancellationToken)
+    {
+        var notesDto = await _serviceManager.NoteService
+            .GetAllByUserIdAsync(userId, cancellationToken);
+        return Ok(notesDto);
+    }
+
+    [HttpGet("{noteId:guid}")]
+    public async Task<IActionResult> GetNoteById(Guid noteId, Guid userId, CancellationToken cancellationToken)
+    {
+        var note = await _serviceManager.NoteService.GetByIdAsync(noteId, userId, cancellationToken);
+        return Ok(note);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateNote(Guid userId,
+        [FromBody] NoteForCreationDto noteForCreationDto,
+        CancellationToken cancellationToken)
+    {
+        var newNote = await _serviceManager.NoteService
+            .CreateNoteAsync(userId ,noteForCreationDto, cancellationToken);
+        
+        return CreatedAtAction(nameof(GetNoteById),
+            new {userId = newNote.UserId, noteId = newNote.Id}, newNote);
+    }
+
+    [HttpPut("{noteId:guid}")]
+    public async Task<IActionResult> UpdateNote(Guid userId, Guid noteId,
+        [FromBody] NoteForUpdateDto noteDto,
+        CancellationToken cancellationToken)
+    {
+        await _serviceManager.NoteService.UpdateNoteAsync(userId, noteId, noteDto, cancellationToken);
+        return NoContent();
+    }
+
+    [HttpDelete("{noteId:guid}")]
+    public async Task<IActionResult> DeleteNote(Guid userId, Guid noteId, CancellationToken cancellationToken)
+    {
+        await _serviceManager.NoteService.DeleteNoteAsync(userId, noteId, cancellationToken);
+        return NoContent();
+    }
+}
---
+++ ToDoApp.Presentation/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoApp.Contracts;
+using ToDoApp.Services.Abstraction;
+
+namespace Presentation.Controllers;
+
+[ApiController]
+[Route("api/user")]
+public class UserController : ControllerBase
+{
+    private readonly IServiceManager _serviceManager;
+
+    public UserController(IServiceManager serviceManager)
+    {
+        _serviceManager = serviceManager;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> RegisterUser(
+        [FromBody] UserForCreationDto userDto,
+        CancellationToken cancellationToken)
+    {
+        var newUser = await _serviceManager.UserService
+            .RegisterNewUser(userDto, cancellationToken);
+        return Ok(newUser);
+    }
+
+    [HttpGet("get-all")]
+    public async Task<IActionResult> GetAllUsers(CancellationToken cancellationToken)
+    {
+        var users = await _serviceManager.UserService.GetAllUsers(cancellationToken);
+        return Ok(users);
+    }
+}
