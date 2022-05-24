dc4ca807360484bd6a1c55cdfb2be0f1fa8579be
Max <pogatop@yandex.ru>
2022-05-24 12:55:50 +0300

    Add configuration for db context

---
+++ ToDoApp.Persistence/Configurations/NoteConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ToDoApp.Domain.Entities;
+
+namespace ToDoApp.Persistence.Configurations;
+
+public class NoteConfiguration : IEntityTypeConfiguration<Note>
+{
+    public void Configure(EntityTypeBuilder<Note> builder)
+    {
+        builder.HasKey(note => note.Id);
+
+        builder.Property(note => note.Id)
+            .ValueGeneratedOnAdd();
+
+        builder.Property(note => note.Name)
+            .IsRequired();
+
+        builder.Property(note => note.Description)
+            .IsRequired();
+
+        builder.Property(note => note.DateCreation)
+            .IsRequired();
+
+        builder.Property(note => note.DateUpdate);
+    }
+}
---
+++ ToDoApp.Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ToDoApp.Domain.Entities;
+
+namespace ToDoApp.Persistence.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(user => user.Id);
+
+        builder.Property(user => user.Id)
+            .ValueGeneratedOnAdd();
+
+        builder.Property(user => user.Username)
+            .IsRequired();
+
+        builder.Property(user => user.Password)
+            .IsRequired();
+
+        builder.Property(user => user.DisplayName)
+            .IsRequired();
+
+        builder.Property(user => user.DateCreation)
+            .IsRequired();
+
+        builder.HasMany(user => user.Notes)
+            .WithOne()
+            .HasForeignKey(note => note.UserId);
+    }
+}
