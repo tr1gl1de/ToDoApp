08a9898bc173d144adf59f93d422922940402d1f
Max <pogatop@yandex.ru>
2022-05-27 22:38:37 +0300

    Add configuration for entities

---
+++ ToDoApp.Entities/Configurations/NoteTypeConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ToDoApp.Entities.Models;
+
+namespace ToDoApp.Entities.Configurations;
+
+public class NoteTypeConfiguration : IEntityTypeConfiguration<Note>
+{
+    public void Configure(EntityTypeBuilder<Note> entity)
+    {
+        entity
+            .HasKey(note => note.Id);
+
+        entity
+            .Property(note => note.Id)
+            .ValueGeneratedOnAdd();
+
+        entity
+            .Property(note => note.Name)
+            .IsRequired();
+
+        entity
+            .Property(note => note.Description);
+
+        entity
+            .Property(note => note.DateCreation)
+            .IsRequired();
+
+        entity
+            .Property(note => note.DateUpdate);
+    }
+}
---
+++ ToDoApp.Entities/Configurations/UserTypeConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ToDoApp.Entities.Models;
+
+namespace ToDoApp.Entities.Configurations;
+
+public class UserTypeConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> entity)
+    {
+        // for real db provider
+        // entity.ToTable("users");
+
+        entity
+            .HasKey(user => user.Id);
+        
+        entity
+            .HasIndex(user => user.Username)
+            .IsUnique();
+        
+        entity
+            .Property(user => user.Id)
+            .IsRequired();
+
+        entity
+            .Property(note => note.Id)
+            .ValueGeneratedOnAdd();
+
+        entity
+            .Property(user => user.Username)
+            .IsRequired();
+
+        entity
+            .Property(user => user.DisplayName)
+            .IsRequired();
+
+        entity
+            .Property(user => user.Password)
+            .IsRequired();
+
+        entity
+            .HasMany(user => user.Notes)
+            .WithOne()
+            .HasForeignKey(note => note.UserId);
+    }
+}
