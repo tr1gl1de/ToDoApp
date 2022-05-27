1d1924031d6e28fb16df2bdda756552197746f09
Max <pogatop@yandex.ru>
2022-05-27 22:07:02 +0300

    Split the Program class into two functional classes

---
+++ ToDoApp.WebApi/Startup.cs
@@ -0,0 +1,32 @@
+﻿namespace ToDoApp.WebApi;
+
+internal class Startup
+{
+    private readonly IConfiguration Configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseRouting();
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints => endpoints.MapControllers());
+    }
+}
