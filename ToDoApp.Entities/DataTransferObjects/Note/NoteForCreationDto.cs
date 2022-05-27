e82af5e9066b17963c1131bc461dc67c9fece458
Max <pogatop@yandex.ru>
2022-05-27 21:05:11 +0300

    Add entities

---
+++ ToDoApp.Entities/DataTransferObjects/Note/NoteForCreationDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApp.Entities.DataTransferObjects.Note;
+
+public class NoteForCreationDto
+{
+    /// <summary>User identifier</summary>
+    /// <example>3B0C6B8E-271A-4CEE-86E8-B0C4B2747316</example>
+    [Required]
+    public Guid UserId { get; set; }
+    
+    /// <summary>Name of note</summary>
+    /// <example>My To-Do list</example>
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    
+    /// <summary>Body of note</summary>
+    /// <example>Do something</example>
+    [Required]
+    public string Description { get; set; } = string.Empty;
+}
---
+++ ToDoApp.Entities/DataTransferObjects/Note/NoteForReadDto.cs
@@ -0,0 +1,20 @@
+﻿namespace ToDoApp.Entities.DataTransferObjects.Note;
+
+public class NoteForReadDto
+{   
+    /// <summary>Name of note</summary>
+    /// <example>My To-Do list</example>
+    public string Name { get; set; } = string.Empty;
+    
+    /// <summary>Body of note</summary>
+    /// <example>Do something</example>
+    public string Description { get; set; } = string.Empty;
+    
+    /// <summary>Date of creation note</summary>
+    /// <example>27.05.2022 17:57:02</example>
+    public DateTime DateCreation { get; set; }
+    
+    /// <summary>Date of creation note</summary>
+    /// <example>28.05.2022 18:10:35</example>
+    public DateTime? DateUpdate { get; set; }
+}
