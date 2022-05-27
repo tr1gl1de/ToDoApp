08ea8e62c2ada11ff17c7fdf6966776337a8edcd
Max <pogatop@yandex.ru>
2022-05-28 01:15:01 +0300

    Update startup

--- ToDoApp.WebApi/Startup.cs
+++ ToDoApp.WebApi/Startup.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using NLog;
 using ToDoApp.Contracts;
 using ToDoApp.Entities;
 using ToDoApp.LoggerService;
+using ToDoApp.Repository;
 
 namespace ToDoApp.WebApi;
 
@@ -12,16 +15,26 @@ internal class Startup
     public Startup(IConfiguration configuration)
     {
         Configuration = configuration;
+        LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/NLog.config"));
     }
 
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddControllers();
         services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen();
+        services.AddSwaggerGen(options =>
+        {
+            var projectDirectory = AppContext.BaseDirectory;
+            var projectName = Assembly.GetExecutingAssembly().GetName().Name;
+            var xmlFileName = $"{projectName}.xml";
+            
+            options.IncludeXmlComments(Path.Combine(projectDirectory,xmlFileName));
+        });
         services.AddDbContext<RepositoryDbContext>(options =>
             options.UseInMemoryDatabase(Guid.NewGuid().ToString()));
         services.AddSingleton<ILoggerManager, LoggerManager>();
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
