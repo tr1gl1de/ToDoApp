6ba7a22c9127635a603a713ef49fe5556ecfb351
Max <pogatop@yandex.ru>
2022-05-31 14:12:56 +0300

    Fix NoteForCreationDto.cs

--- ToDoApp.Entities/DataTransferObjects/Note/NoteForCreationDto.cs
+++ ToDoApp.Entities/DataTransferObjects/Note/NoteForCreationDto.cs
@@ -3,12 +3,7 @@
 namespace ToDoApp.Entities.DataTransferObjects.Note;
 
 public class NoteForCreationDto
-{
-    /// <summary>User identifier</summary>
-    /// <example>3B0C6B8E-271A-4CEE-86E8-B0C4B2747316</example>
-    [Required]
-    public Guid UserId { get; set; }
-    
+{   
     /// <summary>Name of note</summary>
     /// <example>My To-Do list</example>
     [Required]
