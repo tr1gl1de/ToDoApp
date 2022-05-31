901bad8eaaa1f579db2e8ad2098d505aefd0f99e
Max <pogatop@yandex.ru>
2022-05-31 22:00:08 +0300

    Add XML comments specification in NoteController.cs

--- ToDoApp.WebApi/Controllers/NoteController.cs
+++ ToDoApp.WebApi/Controllers/NoteController.cs
@@ -24,7 +24,13 @@ public class NoteController : ControllerBase
         _mapper = mapper;
     }
 
+    /// <summary>Create a new note.</summary>
+    /// <param name="noteCreate">Object Note.</param>
+    /// <response code="201">Created note.</response>
+    /// <response code="404">User not found.</response> 
     [HttpPost]
+    [ProducesResponseType(typeof(NoteForReadDto), StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> CreateNote([FromBody] NoteForCreationDto noteCreate)
     {
         var subClaim = User.Claims.Single(claim => claim.Type == "sub");
@@ -48,7 +54,15 @@ public class NoteController : ControllerBase
             newNote);
     }
 
+    /// <summary>Get note with id.</summary>
+    /// <param name="id">Identifier of note.</param>
+    /// <response code="200">Note received.</response>
+    /// <response code="404">Note not found.</response>
+    /// <response code="409">Note does not belong to user.</response> 
     [HttpGet("{id:guid}")]
+    [ProducesResponseType(typeof(NoteForReadDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
     public async Task<IActionResult> GetNoteById([FromRoute] Guid id)
     {
         var subClaim = User.Claims.Single(claim => claim.Type == "sub");
@@ -62,14 +76,19 @@ public class NoteController : ControllerBase
 
         if (note.UserId != userId)
         {
-            return Conflict("This note does not belong to you");
+            return Conflict("This note does not belong to user");
         }
         
         var readNote = _mapper.Map<NoteForReadDto>(note);
         return Ok(readNote);
     }
 
+    /// <summary>Get all user notes.</summary>
+    /// <response code="200">Notes received.</response>
+    /// <response code="404">User not found.</response> 
     [HttpGet("all")]
+    [ProducesResponseType(typeof(NoteForReadDto[]), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetUserNotes()
     {
         var subClaim = User.Claims.Single(claim => claim.Type == "sub");
@@ -87,7 +106,16 @@ public class NoteController : ControllerBase
         return Ok(userNotesRead);
     }
 
+    /// <summary>Update note with id.</summary>
+    /// <param name="noteId">Identifier of note.</param>
+    /// <param name="noteForUpdateDto">Object note.</param>
+    /// <response code="204">Updated note.</response>
+    /// <response code="404">Note not found.</response>
+    /// <response code="409">Note does not belong to user.</response> 
     [HttpPut("{noteId:guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
     public async Task<IActionResult> UpdateNote([FromRoute]Guid noteId, [FromBody] NoteForUpdateDto noteForUpdateDto)
     {
         var subClaim = User.Claims.Single(claim => claim.Type == "sub");
@@ -112,6 +140,11 @@ public class NoteController : ControllerBase
         return NoContent();
     }
 
+    /// <summary>Delete note with id.</summary>
+    /// <param name="noteId">Identifier of note.</param>
+    /// <response code="204">Deleted note.</response>
+    /// <response code="404">Note not found.</response>
+    /// <response code="409">Note does not belong to user.</response>
     [HttpDelete("{noteId:guid}")]
     public async Task<IActionResult> DeleteNote([FromRoute] Guid noteId)
     {
